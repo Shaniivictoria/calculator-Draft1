aea147c17d94df08b723bb43c8659af899f46547
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-02-03 09:14:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const resultTab= document.getElementById("resultTab")
+const displayBtn7= document.getElementById("displayBtn7")
+
+displayBtn7.addEventListener('click', ()=>{
+    result.innerHTML = 7
+    })
+
+    console.log(resultTab);
